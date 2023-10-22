8e1b6b54e25c174f45adcf0864c439ab3a5aef30
Gayathri-babusuresh <144888407+Gayathri-babusuresh@users.noreply.github.com>
2023-10-22 18:02:47 +0530

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -7,41 +7,41 @@
     var indexPwd = localStorage.getItem("pwd");
 
     if (user == "") {
-      document.getElementById("username").innerHTML =
+      document.getElementById("user").innerHTML =
         " ** Please fill the username field";
       return false;
     }
     if (user.length <= 2 || user.length > 20) {
-      document.getElementById("username").innerHTML =
+      document.getElementById("user").innerHTML =
         " ** Username lenght must be between 2 and 20";
       return false;
     }
     if (!isNaN(user)) {
-      document.getElementById("username").innerHTML =
+      document.getElementById("user").innerHTML =
         " ** only characters are allowed";
       return false;
     } 
 
     if (user != indexUser){
-      document.getElementById("username").innerHTML=
+      document.getElementById("user").innerHTML=
       " ** username is invalid";
       return false;
     }
 
     if (pass == "") {
-      document.getElementById("passwords").innerHTML =
+      document.getElementById("pass").innerHTML =
         " ** Please fill the password field";
       return false;
     }
     if (pass.length <= 5 || pass.length > 20) {
-      document.getElementById("passwords").innerHTML =
+      document.getElementById("pass").innerHTML =
         " ** Passwords lenght must be between  5 and 20";
       return false;
     }
 
     if (pass != indexPwd) {
       document.getElementById("pass").innerHTML =
-        " ** Password does not match the confirm password";
+        " ** Invalid Password";
       return false;
 
     }
